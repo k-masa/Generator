bd08fad6cc8cff00f6050525532cba38494333e6
k-masa <wyrbtt@gmail.com>
2015-05-24 08:43:32 +0900

    Initial Commit

---
+++ Template/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Template
+//
+//  Created by Kato Masaya on 5/24/15.
+//  Copyright (c) 2015 Kato Masaya. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
